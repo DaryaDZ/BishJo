7907a2fabbb6acb8b881c4d29e298a9933c75c23
YasNvd <yaasnvd@yahoo.com>
2023-03-01 00:49:25 +0330

    :)

--- src/Panel/PanelsLayout.tsx
+++ src/Panel/PanelsLayout.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import PanelsNavbar from './PanelsNavbar'
 import { Outlet } from "react-router-dom";
+import Manu from "../Assets/ICONS/Menu.png"
+import './Panel.css'
+
 
 
 function PanelsLayout() {
   return (
     <div style={{backgroundColor: "#E9F2FB", height: "900px", display: "flex", justifyContent: "center", alignItems: "center", }}>
       <div style={{width:"1000px", height: "780px", backgroundColor: "#FFFFFF", borderRadius: "24px" }}>
+        <div> 
+          <img className='panelssidebar'  src={Manu} /> 
+        </div>
        < PanelsNavbar/>
       < Outlet />
       </div>
